74ed5a0fead66ba868ab14fc5faf58c9e589ac26
d8xa <>
2021-07-23 05:32:46 +0200

    removed convert.sh

--- data/convert.sh
+++
@@ -1,9 +0,0 @@
-for filepath in doc/*; do
-    filename=$(basename -- "$filepath")
-    extension="${filename##*.}"
-    #fname="${filename%.*}"
-    outpath="txt/${fname}.txt"
-    textutil -convert txt "$filepath" -output "$outpath"
-    echo "converted $filename."
-done
-echo "done."
